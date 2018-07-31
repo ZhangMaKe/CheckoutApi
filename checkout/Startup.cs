f868bb7b935b46dd38dcf26a89463abb448b0e80
mark.faiers <mark.faiers@spdspark.com>
2018-07-31 10:41:03 +0100

    updated lauch settings, added swagger, removed further unecessary using statements. added documentation.

--- checkout/Startup.cs
+++ checkout/Startup.cs
@@ -1,15 +1,10 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using checkout.Models;
+﻿using checkout.Models;
 using checkout.Repositories;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace checkout
 {
@@ -29,7 +24,10 @@ namespace checkout
 
             services.AddSingleton<IRepository<Order>, OrderRepository>();
 
-            //services.AddScoped<IRepository<Order>, OrderRepository>();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info {Title = "CheckoutAPI", Version = "v1"});
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -39,6 +37,14 @@ namespace checkout
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwagger();
+
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "Checkout API v1");
+                c.RoutePrefix = string.Empty;
+            });
 
             app.UseMvc();
         }
